6bdb4c7ebf4993793f23d6c52fa93bc070861fa0
HunterS <schneider6391@gmail.com>
2018-03-26 20:02:17 -0400

    fix google maps api code and get user location

--- jss/main.js
+++ jss/main.js
@@ -38,27 +38,42 @@ function loadMarkers() {
 
 //HOW TO CREATE A MAP
 function initMap() {
-    map_options = {
-      zoom: 13,
-         //above changes zoom of map - higher the number, closer to the point
-      mapTypeId: google.maps.MapTypeId.roadmap,
-         //above changes type of map - ex satellite, roadmap, etc
-      center: {lat: 40.738565, lng: -73.976958}
-         //above sets center of the map
-    }
-    
-    map_document = document.getElementById('map')
-          //grabs value of div with ID name of 'map' and assigns it to map_document
-    map = new google.maps.Map(map_document,map_options);
-        //using Map constructor- pass map_options and map div (aka map_document) to create 'map' object
-    geojson_url = 'https://raw.githubusercontent.com/gizm00/blog_code/master/appendto/python_maps/collection.geojson'
+  map_options = {
+    zoom: 13,
+       //above changes zoom of map - higher the number, closer to the point
+    mapTypeId: google.maps.MapTypeId.roadmap,
+       //above changes type of map - ex satellite, roadmap, etc
+    center: {lat: 40.738565, lng: -73.976958}
+       //above sets center of the map
+  }
+  
+  map_document = document.getElementById('map')
+        //grabs value of div with ID name of 'map' and assigns it to map_document
+  map = new google.maps.Map(map_document,map_options);
+      //using Map constructor- pass map_options and map div (aka map_document) to create 'map' object
+  geojson_url = "../json/main.json";
+  console.log('loading geojson')
+  map.data.loadGeoJson(geojson_url, null, loadMarkers) ;
 
-    console.log('loading geojson')
-    map.data.loadGeoJson(geojson_url, null, loadMarkers) 
- 
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+// google.maps.event.addDomListener(window, 'load', initMap);
   //means that the initMap function will be called when the window is loaded
 
 //-------
@@ -101,5 +116,5 @@ google.maps.event.addDomListener(window, 'load', initMap);
  //        infoWindow.open(map);
  //      }
 
-
+ 
 
