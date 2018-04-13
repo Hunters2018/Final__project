2e866b8ca22fd618d4ac54a0714d059f0b91fbbf
HunterS <schneider6391@gmail.com>
2018-04-12 20:03:15 -0400

    test

--- jss/main.js
+++ jss/main.js
@@ -97,7 +97,7 @@ function loadMarkers() {
       map: map
      });
     
-    var content = "<div><h3>" + titleText + "</h3><p>Performers: " + descriptionText + "</p></div>" + cityText + timeText + addressText
+    var content = "<div><h3>" + titleText + "</h3><p>Performers: " + descriptionText + "</p></div>"
     
 
     // by default the infoWindow for each marker will stay open unless manually closed
